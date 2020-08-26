f251eca708a3a0f0b3f96d17a8f944d4e56d977b
Toni-d-e-v <62844491+Toni-d-e-v@users.noreply.github.com>
2020-08-26 21:06:59 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Ready')
+
+
+
+while True:
+    print ('its working')
+    @client.event
+    async def on_ready():
+        print('https://github.com/search?o=desc&q=blockchain&s=updated&type=Repositories')
+    
+    
+
+    time.sleep(100)
+    
+client.run('720707364067606558')
